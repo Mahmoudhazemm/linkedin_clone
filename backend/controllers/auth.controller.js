b83039f846c2ca47be5b4090971fcb0f2e6afec1
Mahmoudhazemm <mahmoudhazem62004@gmail.com>
2026-08-15 17:55:51 +0300

    signup route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,58 @@
-export const signup = (req, res) => {
-  res.send("signup");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, username, email, password } = req.body;
+    if (!name || !username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      name,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({ message: "User created successfully" });
+
+    // TODO: Send verification email
+  } catch (error) {
+    console.error("Error during signup:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //parse JSON request bodies
+
 app.use("/api/v1/auth", authRoutes);
+
+console.log("PORT =", PORT);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
